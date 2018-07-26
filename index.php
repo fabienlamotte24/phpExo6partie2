a893dfbbc0a20888dd0bd3e3411bf16427fbfa2f
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 16:09:51 +0200

    exo6p2 php

--- index.php
+++ index.php
@@ -7,10 +7,12 @@
   <body>
     <p>
 <?php
+/*Variable*/
   $age = 18;
+  /*Condition*/
   if($age >= 18){
     echo 'tu es majeur';
-  } else {
+  } else {/*Autre condition si n'est pas majeur*/
     echo 'tu n\'es pas majeur';
   }
  ?>
