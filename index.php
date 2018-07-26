500fe43830cd4f52c5418d2f2d53bcd891507121
fabienlamotte24 <fabien.lamotte00@outlook.fr>
2018-07-26 09:38:17 +0200

    exo6 p2 php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 6</title>
+  </head>
+  <body>
+    <p>
+<?php
+  $age = 18;
+  if($age >= 18){
+    echo 'tu es majeur';
+  } else {
+    echo 'tu n\'es pas majeur';
+  }
+ ?>
+    </p>
+  </body>
+</html>
